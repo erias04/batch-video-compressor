b541f0bd44e31c2281f0658109f256cd1f0aef76
erias04 <lichtbargeld@gmail.com>
2021-11-29 22:44:43 +0000

    added multiple formats .rstrip() is used to get the free space from the file away. A space at the end breaks glob so it will be removed automatcally

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import glob/Volumes/erias-macos/Users/eliascerne/Documents/batchVideoConverter/main.py
 
 file_path = input('Which folder do you want to process?\n').rstrip();
-types = ['.mp4', '.MP4']
+types = ['.mp4', '.MP4', '.mov', '.MOV']
 files = []
 
 for type in types:
